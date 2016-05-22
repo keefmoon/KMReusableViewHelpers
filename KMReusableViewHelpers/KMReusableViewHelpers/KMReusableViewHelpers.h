1a08a2df2e06179417f33786503633f858274174
Keith Moon <keef@keefmoon.com>
2016-05-22 22:14:18 +0100

    Initial commit of code

---
+++ KMReusableViewHelpers/KMReusableViewHelpers/KMReusableViewHelpers.h
@@ -0,0 +1,19 @@
+//
+//  KMReusableViewHelpers.h
+//  KMReusableViewHelpers
+//
+//  Created by Keith Moon on 22/05/2016.
+//  Copyright © 2016 Data Ninjitsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMReusableViewHelpers.
+FOUNDATION_EXPORT double KMReusableViewHelpersVersionNumber;
+
+//! Project version string for KMReusableViewHelpers.
+FOUNDATION_EXPORT const unsigned char KMReusableViewHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMReusableViewHelpers/PublicHeader.h>
+
+
---
+++ KMReusableViewHelpers/KMReusableViewHelpers/Swift/UIView+NibLoading.swift
@@ -0,0 +1,53 @@
+//
+//  UIView+NibLoading.swift
+//  KMReusableViewHelpers
+//
+//  Created by Keith Moon on 06/11/2015.
+//  MIT Licence
+//
+
+import UIKit
+
+
+/// Protocol to help with loading a view from a Nib.
+/// NOTE on implementing: Views that implement this must be declared final.
+/// The default implementation is that the nibName matches the class name, 
+/// only situations other than this need to be explicitly declared.
+protocol NibLoadable {
+    
+    /// Nib name
+    static var nibName: String { get }
+    
+    /// Load the nib
+    static func nib() -> UINib
+    
+    /// Load view from associated nib
+    /// - returns: The inflated view
+    static func loadFromNib() -> Self
+    
+}
+
+
+/// Default implementation
+/// nibName matches the class name, only situations other than this need to be explicitly declared.
+extension NibLoadable {
+    
+    static var nibName: String {
+        return String(self)
+    }
+    
+}
+
+
+extension NibLoadable where Self: UIView {
+    
+    static func nib() -> UINib {
+        return UINib(nibName: self.nibName, bundle: nil)
+    }
+    
+    
+    static func loadFromNib() -> Self {
+        return nib().instantiateWithOwner(self, options: nil).first as! Self
+    }
+
+}
